82d0cabf1f9d65481b83c741c27ecb0a8d393b00
Morgan Bonar <mbonar@gmail.com>
2017-05-25 06:11:01 -0700

    adding basic express router

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes/index');
 
 app.set('port', process.env.PORT || 6161);
 
-app.get('/', (req,res) => {
-  res.send('<h1>Hello world!</h1>');
-})
+app.use('/', routes);
 
 app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${app.get('port')}`);
